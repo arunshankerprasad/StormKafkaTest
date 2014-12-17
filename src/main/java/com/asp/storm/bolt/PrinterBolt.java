9b7451267872376f79c5809bd90d646b53fc8daa
Arun Shanker Prasad <arunshanker@qburst.com>
2014-12-17 17:54:43 +0530

    Initial commit

---
+++ src/main/java/com/asp/storm/bolt/PrinterBolt.java
@@ -0,0 +1,35 @@
+package com.asp.storm.bolt;
+
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+public class PrinterBolt extends BaseRichBolt {
+
+    /**
+     * Auto-generated serial version UID.
+     */
+    private static final long serialVersionUID = -79691469992447756L;
+
+    private OutputCollector collector;
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        System.out.println(input.toString());
+        collector.fail(input);
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public void prepare(Map arg0, TopologyContext arg1, OutputCollector collector) {
+        this.collector = collector;
+    }
+}
---
+++ src/main/java/com/asp/storm/topology/KafkaSpoutTestTopology.java
@@ -0,0 +1,59 @@
+package com.asp.storm.topology;
+
+import java.util.Arrays;
+
+import storm.kafka.BrokerHosts;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.StormTopology;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.topology.TopologyBuilder;
+
+import com.asp.storm.bolt.PrinterBolt;
+
+public class KafkaSpoutTestTopology {
+    private final BrokerHosts brokerHosts;
+
+    public KafkaSpoutTestTopology(String kafkaZookeeper) {
+        brokerHosts = new ZkHosts(kafkaZookeeper);
+    }
+
+    public StormTopology buildTopology() {
+        SpoutConfig kafkaConfig = new SpoutConfig(brokerHosts, "storm-sentence", "", "storm");
+        kafkaConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("words", new KafkaSpout(kafkaConfig), 10);
+        builder.setBolt("print", new PrinterBolt()).shuffleGrouping("words");
+        return builder.createTopology();
+    }
+
+    public static void main(String[] args) throws Exception {
+        String kafkaZk = args[0];
+        KafkaSpoutTestTopology kafkaSpoutTestTopology = new KafkaSpoutTestTopology(kafkaZk);
+        Config config = new Config();
+        config.put(Config.TOPOLOGY_TRIDENT_BATCH_EMIT_INTERVAL_MILLIS, 2000);
+
+        StormTopology stormTopology = kafkaSpoutTestTopology.buildTopology();
+        if (args != null && args.length > 1) {
+            String name = args[1];
+            String dockerIp = args[2];
+            config.setNumWorkers(2);
+            config.setMaxTaskParallelism(5);
+            config.put(Config.NIMBUS_HOST, dockerIp);
+            config.put(Config.NIMBUS_THRIFT_PORT, 6627);
+            config.put(Config.STORM_ZOOKEEPER_PORT, 2181);
+            config.put(Config.STORM_ZOOKEEPER_SERVERS, Arrays.asList(dockerIp));
+            StormSubmitter.submitTopology(name, config, stormTopology);
+        } else {
+            config.setNumWorkers(2);
+            config.setMaxTaskParallelism(2);
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("kafka", config, stormTopology);
+        }
+    }
+}
